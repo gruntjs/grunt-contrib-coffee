fd1727474b4c4b358362ed13f91a996497921072
Chris Talkington <chris@talkingtontech.com>
2012-12-12 17:33:48 -0600

    cleanup after merge.

--- docs/coffee-examples.md
+++ docs/coffee-examples.md
@@ -1,14 +1,20 @@
 # Usage Examples
 
-``` javascript
+```js
 coffee: {
   compile: {
     files: {
       'path/to/result.js': 'path/to/source.coffee', // 1:1 compile
       'path/to/another.js': ['path/to/sources/*.coffee', 'path/to/more/*.coffee'] // compile and concat into single file
     }
+  },
+
+  glob_to_multiple: {
+    files: grunt.file.expandMapping(['path/to/*.coffee'], 'path/to/dest/', {
+      rename: function(destBase, destPath) {
+        return destBase + destPath.replace(/\.coffee$/, '.js');
+      }
+    })
   }
 }
 ```
-
-check out this [gist](https://gist.github.com/3703920) if your looking to compile each file in a directory individually. grunt may include such a utility in future versions but for now the gist allows any task that supports 1:1 compiling to support dynamic 1:1 compiling based on glob.
--- test/coffee_test.js
+++ test/coffee_test.js
@@ -1,18 +1,30 @@
 var grunt = require('grunt');
 var fs = require('fs');
 
+function readFile(file) {
+  'use strict';
+
+  var contents = grunt.file.read(file);
+
+  if (process.platform === 'win32') {
+    contents = contents.replace(/\r\n/g, '\n');
+  }
+
+  return contents;
+}
+
 exports.coffee = {
   compile: function(test) {
     'use strict';
 
     test.expect(2);
 
-    var actual = grunt.file.read('tmp/coffee.js');
-    var expected = grunt.file.read('test/expected/coffee.js');
+    var actual = readFile('tmp/coffee.js');
+    var expected = readFile('test/expected/coffee.js');
     test.equal(expected, actual, 'should compile coffeescript to javascript');
 
-    actual = grunt.file.read('tmp/concat.js');
-    expected = grunt.file.read('test/expected/concat.js');
+    actual = readFile('tmp/concat.js');
+    expected = readFile('test/expected/concat.js');
     test.equal(expected, actual, 'should compile multiple coffeescript files to a single javascript file');
 
     test.done();
