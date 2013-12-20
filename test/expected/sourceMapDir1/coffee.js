8a69fead9df5eb9cd3e62fc44485e7a40bf1bad7
Johnny Freeman <prsjohnny@gmail.com>
2013-12-20 13:15:05 -0500

    added a compilation step in Gruntfile, a test suite, and expected results for the new sourceMapDir option

---
+++ test/expected/sourceMapDir1/coffee.js
@@ -0,0 +1,17 @@
+(function() {
+  var HelloWorld;
+
+  HelloWorld = (function() {
+    function HelloWorld() {}
+
+    HelloWorld.test = 'test';
+
+    return HelloWorld;
+
+  })();
+
+}).call(this);
+
+/*
+//# sourceMappingURL=../../tmp/sourceMapDir2/coffee.js.map
+*/
