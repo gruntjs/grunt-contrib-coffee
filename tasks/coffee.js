c6430751313030b0db48e6b8dbc98848dbd49c8b
XhmikosR <xhmikosr@gmail.com>
2016-04-14 23:39:08 +0300

    Remove trailing spaces.
    [ci skip]

--- tasks/coffee.js
+++ tasks/coffee.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 
     this.files.forEach(function(f) {
       var validFiles = removeInvalidFiles(f);
-      
+
       if (validFiles.length === 0) {
         grunt.log.warn('Destination ' + chalk.cyan(f.dest) + ' not written because no source files were found.');
         return;
